3ff3d54589cda212d19b04333ab199a17a820376
Jorge Valdez <georch@gmail.com>
2018-01-13 17:39:42 -0600

    Add Help Block component

---
+++ src/components/HelpBlock.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="help is-size-7">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelpBlock',
+};
+</script>
+
+<style scoped>
+.help {
+  margin: .5em 0 1em;
+  padding: .5em 1em;
+  color: #8a6d3b;
+  background: #fcf8e3;
+  border: 1px solid #faebcc;
+  border-radius: 4px;
+}
+</style>
