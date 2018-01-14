2c037f054687f17e84e3eb28987811435c4f5189
Jorge Valdez <georch@gmail.com>
2018-01-14 02:15:24 -0600

    Add Logout component

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -5,13 +5,24 @@
         <router-link class="navbar-item is-tab" to="/" exact>
           Inicio
         </router-link>
+        <router-link v-if="isLogged" class="navbar-item is-tab" to="/logout" exact>
+          Terminar sesión
+        </router-link>
       </div>
     </div>
   </nav>
 </template>
 
 <script>
+import store from '@/store';
+
 export default {
   name: 'NavBar',
+
+  computed: {
+    isLogged() {
+      return store.state.isLogged;
+    },
+  },
 };
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Login from '@/components/authentication/Login';
+import Logout from '@/components/authentication/Logout';
 import Welcome from '@/components/Welcome';
 
 Vue.use(Router);
@@ -18,6 +19,11 @@ export default new Router({
       path: '/login',
       name: 'Login',
       component: Login,
+    },
+    {
+      path: '/logout',
+      name: 'Logout',
+      component: Logout,
     },
   ],
   linkExactActiveClass: 'is-active',
