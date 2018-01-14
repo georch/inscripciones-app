e01ffa6a171c5c9301d796233cae864013b0850e
Jorge Valdez <georch@gmail.com>
2018-01-13 21:16:20 -0600

    Define Font Awesome library

--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import fontawesome from '@fortawesome/fontawesome';
+import {
+  faEnvelope,
+  faLock,
+} from '@fortawesome/fontawesome-free-solid';
+
 import Vue from 'vue';
 import App from './App';
 import router from './router';
+
+fontawesome.library.add(faEnvelope, faLock);
 
 // Require the main Sass manifest file
 require('./assets/sass/main.scss');
